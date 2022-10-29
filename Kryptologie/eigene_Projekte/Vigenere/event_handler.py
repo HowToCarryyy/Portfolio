eff727993bdcce839b92d4100342404c82d6d5f2
simonlsr <89278229+simonlsr@users.noreply.github.com>
2022-10-29 11:39:39 +0200

    tasks corrected & multiplikative Inverses

--- Kryptologie/eigene_Projekte/Vigenere/event_handler.py
+++ Kryptologie/eigene_Projekte/Vigenere/event_handler.py
@@ -1,26 +1,26 @@
 from js import document
 
-import Caesar as c
+import Vigenere as v
 
 
-def simple_caesar_encryption(event):
-    text_input = document.getElementById("simple_caesar_encryption_text")
-    number_input = document.getElementById("simple_caesar_encryption_number")
-    encrypted_text = c.simple_caesar_encryption(text_input.value, int(number_input.value))
+def vigenere_encryption(event):
+    text_input = document.getElementById("vigenere_encryption_text")
+    key_input = document.getElementById("vigenere_encryption_key")
+    encrypted_text = v.vigenere_encryption(text_input.value.upper(), key_input.value.upper())
 
-    document.getElementById("simple_caesar_decryption_text").value = encrypted_text
-    document.getElementById("simple_caesar_decryption_number").value = int(number_input.value)
+    document.getElementById("vigenere_decryption_text").value = encrypted_text.upper()
+    document.getElementById("vigenere_decryption_key").value = key_input.value.upper()
 
-    output = document.getElementById("simple_caesar_encryption_output")
+    output = document.getElementById("vigenere_encryption_output")
     output.style.display = "block"
     final = "Verschlüsselter Text: " + encrypted_text
-    Element("simple_caesar_encryption_output").write(final)
+    Element("vigenere_encryption_output").write(final)
 
-def simple_caesar_decryption(event):
-    text_input = document.getElementById("simple_caesar_decryption_text")
-    number_input = document.getElementById("simple_caesar_decryption_number")
-    decrypted_text = c.simple_caesar_decryption(text_input.value, int(number_input.value))
-    output = document.getElementById("simple_caesar_decryption_output")
+def vigenere_decryption(event):
+    text_input = document.getElementById("vigenere_decryption_text")
+    key_input = document.getElementById("vigenere_decryption_key")
+    decrypted_text = v.vigenere_decryption(text_input.value.upper(), key_input.value.upper())
+    output = document.getElementById("vigenere_decryption_output")
     output.style.display = "block"
     final = "Entschlüsselter Text: " + decrypted_text
-    Element("simple_caesar_decryption_output").write(final)
+    Element("vigenere_decryption_output").write(final)
--- src/Projekte/Vigenere/Vigenere.py
+++ src/Projekte/Vigenere/Vigenere.py
@@ -1,7 +1,7 @@
-def vigenere_encrypt(input, key):
+def vigenere_encryption(input, key):
     output = [" " if x == " " else chr((ord(input[x]) + (ord(key[x % len(key)]) - 65) - 65) % 26 + 65) for x in range(len(input))]
     return "".join(output)
 
-def vigenere_decrypt(input, key):
+def vigenere_decryption(input, key):
     output = [" " if x == " " else chr((ord(input[x]) - (ord(key[x % len(key)]) - 65) - 65) % 26 + 65) for x in range(len(input))]
     return "".join(output)
